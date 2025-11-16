e2a34413a3ddeb097696814ecdbc6a2a48bbf0d0
Victor Hugo <victorhugof.lisboa@gmail.com>
2025-11-16 09:09:49 -0300

    correcao deteccao mao

--- processamento/classifica_processa_video.py
+++ processamento/classifica_processa_video.py
@@ -20,7 +20,7 @@ def process_and_label_video(input_video_path, output_video_path, output_csv_path
     out_gray = None
 
     mp_hands = mp.solutions.hands
-    hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5)
+    hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5)
     mp_drawing = mp.solutions.drawing_utils
 
     labeled_rows = []  # [frame_idx, label]
